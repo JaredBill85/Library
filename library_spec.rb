42671623f2c77a6d559e57304d57b91b06d9db3e
JaredBill85 <JaredBill@Gmail.com>
2014-03-23 19:36:34 -0400

    Refactored using lets to eliminate duplication

--- library_spec.rb
+++ library_spec.rb
@@ -3,42 +3,35 @@ require_relative 'user.rb'
 require_relative 'book.rb'
 
 describe Library do
-  it 'User can check-out books ' do
-    user = User.new
-    book = Book.new
+  let (:user) { User.new }
+  let (:book) { Book.new }    
     
+  it 'User can check-out books ' do
     user.CheckOut(book)
     book.Available.should be_true
-   end
+  end
    
-    it 'User can check-in books ' do
-    user = User.new
-    book = Book.new
-    
+  it 'User can check-in books ' do
     user.CheckOut(book)
     user.CheckIn(book)
     book.Available.should be_false
-   end 
+  end 
 end
 
 describe Book do
+
+  let(:book) { Book.new }
   it 'has a Genre' do
-    book = Book.new
-    
     book.Genre = "Sci-fi"
     book.Genre.should eq("Sci-fi")
   end
   
   it 'has a Format' do
-    book = Book.new
-    
     book.Format = "Hardcopy"
     book.Format.should eq("Hardcopy")
   end
   
   it 'has a Language' do
-    book = Book.new
-    
     book.Language = "Pittsburgese"
     book.Language.should eq("Pittsburgese")
   end
